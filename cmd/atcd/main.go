b1568594ada09fa2b03a57920a8f600cb9c36c45
savaki <matt.ho@gmail.com>
2015-07-14 17:53:04 -0700

    - added support for docker-compose

--- cmd/atcd/main.go
+++ cmd/atcd/main.go
@@ -10,20 +10,27 @@ import (
 	"github.com/codegangsta/cli"
 )
 
+const (
+	ENV_SQL_DATASOURCE = "ATC_SQL_DATASOURCE"
+	ENV_SQL_DRIVER     = "ATC_SQL_DRIVER"
+)
+
 var atcFlags = []cli.Flag{
 	cli.BoolFlag{"dev", "dev mode; lax security", "ATC_DEV"},
 	cli.StringFlag{"callbacksURL", "http://127.0.0.1:8080", "URL used for callbacks to reach the ATCD (excluding basic auth)", "ATC_CALLBACK_URL"},
 	cli.StringFlag{"checkInterval", "1m0s", "interval on which to poll for new versions of resources", "ATC_CHECK_INTERVAL"},
 	cli.StringFlag{"httpUsername", "", "basic auth username for the server", "ATC_USERNAME"},
 	cli.StringFlag{"httpPassword", "", "basic auth password for the server", "ATC_PASSWORD"},
-	cli.StringFlag{"sqlDataSource", "postgres://127.0.0.1:5432/atc?sslmode=disable", "database/sql data source configuration string", "ATC_SQL_DATASOURCE"},
-	cli.StringFlag{"sqlDriver", "postgres", "database/sql driver name", "ATC_SQL_DRIVER"},
+	cli.StringFlag{"sqlDataSource", "postgres://127.0.0.1:5432/atc?sslmode=disable", "database/sql data source configuration string", ENV_SQL_DATASOURCE},
+	cli.StringFlag{"sqlDriver", "postgres", "database/sql driver name", ENV_SQL_DRIVER},
 	cli.StringFlag{"public", "web/public", "path to directory containing public resources (javascript, css, etc.)", "ATC_PUBLIC"},
 	cli.StringFlag{"templates", "web/templates", "path to directory containing the html templates", "ATC_TEMPLATES"},
 	cli.IntFlag{"webListenPort", 8080, "port for the web server to listen on", "ATC_PORT"},
 }
 
 func main() {
+	bindLinkedDockerDataSource()
+
 	app := cli.NewApp()
 	app.Flags = []cli.Flag{
 		cli.StringFlag{"atc", "atc", "path to atc command", "ATC_ATC"},
@@ -37,6 +44,28 @@ func main() {
 		},
 	}
 	app.Run(os.Args)
+}
+
+func bindLinkedDockerDataSource() {
+	if os.Getenv(ENV_SQL_DATASOURCE) != "" {
+		return
+	}
+
+	log.Println("scanning for linked docker container named 'db'")
+
+	user := "postgres"
+	if v := os.Getenv("DB_ENV_POSTGRES_USER"); v != "" {
+		user = v
+	}
+
+	password := os.Getenv("DB_ENV_POSTGRES_PASSWORD")
+	ipAddr := os.Getenv("DB_PORT_5432_TCP_ADDR")
+
+	if user != "" && password != "" && ipAddr != "" {
+		log.Printf("found container, db.  updating %s\n", ENV_SQL_DATASOURCE)
+		dataSource := fmt.Sprintf("postgres://%s:%s@%s:5432/atc?sslmode=disable", user, password, ipAddr)
+		os.Setenv(ENV_SQL_DATASOURCE, dataSource)
+	}
 }
 
 func makeArgs(c *cli.Context) []string {
